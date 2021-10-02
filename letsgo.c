3b06f1b7693937329a246a15f1f8645930e33abb
USERNAME <EMAIL>
2021-10-02 09:21:27 +0200

    initial version

---
+++ letsgo.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <windows.h>
+#include <time.h>
+
+#define quad 103
+#define r 1
+#define c 1
+
+char punto[1]{quad};
+char lati[r][c];
+
+
+void fill(){
+	int i,j;
+	if(punto[1]==quad){
+		for(i=0;i<10;i++){
+			for(j=0;j<10;j++){
+				 lati[i][j]=103;
+				 printf()
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+int main(){
+	
+	fill();
+	
+
+
+
+
+
+
+	return 0;
+}
